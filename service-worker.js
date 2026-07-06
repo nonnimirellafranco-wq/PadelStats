aaf9325e529b1a527bc483bf39c5614d46677e1e
nonnimirellafranco-wq <nonnimirellafranco@gmail.com>
2026-07-06 11:26:25 +0200

    Add files via upload
    Padel Stats v2.3 Stable
    
    - migliorata gestione della cache PWA
    - aggiunte icone manifest (192/512)
    - corretto bug ID duplicati Share Card

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,68 @@
-const CACHE_NAME = "padelstats-v3";
+const CACHE_NAME = "padelstats-v2.3.0";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./themes.css",
-  "./script.js",
-  "./manifest.json"
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./themes.css",
+    "./script.js",
+    "./manifest.json",
+
+    "./icon-192.png",
+    "./icon-512.png",
+
+    "./Immagini/logoHeader.png",
+    "./Immagini/logoHeader2.png",
+    "./Immagini/logoapp.png"
 ];
 
+// Installazione
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
 });
 
+// Attivazione
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+
+                })
+
+            );
+
+        }).then(() => self.clients.claim())
+
+    );
+
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+
+    );
+
 });
